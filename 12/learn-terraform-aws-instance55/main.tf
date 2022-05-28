11d9ea14772aef8755e00bdb60bf809cb2a43e0e
Vadym Beznosenko <vadim.beznosenko88@gmail.com>
2022-05-28 20:55:35 +0300

    task12

---
+++ 12/learn-terraform-aws-instance55/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "vadimbeznosenko"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
